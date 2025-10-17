f04c5a3545dc30247a22cd56cb6f66a7458bddc2
Syed Asad Jamil <syedasad1410@gmail.com>
2025-10-17 22:10:55 +0500

    error resolved

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,10 @@
 // src/api/api.js
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
+// const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
+
+const API_BASE = "https://edm-website-backend-production.up.railway.app";
+
 
 const api = axios.create({
     baseURL: `${API_BASE}/api`,
